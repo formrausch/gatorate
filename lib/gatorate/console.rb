7f7635540ffdca8df0d1272bbe2f5bf6afb680e2
Thomas Winkler <tom@formrausch.com>
2014-01-09 15:06:23 +0100

    some nice helper methods

--- lib/gatorate/console.rb
+++ lib/gatorate/console.rb
@@ -12,14 +12,33 @@ module Gatorate
       DCell::Node.find("web")
     end
 
+    def observer
+      DCell::Node.find("observe")
+    end
+
+    def actor(actor_name)
+      observer.find(actor_name.to_sym)
+    end
+
     def door_actor
-      @door_node  = DCell::Node.find("observe")
-      @door_node.find(:door)
+      actor(:door)
     end
 
     def heartbeat_actor
-      @door_node  = DCell::Node.find("observe")
-      @door_node.find(:heartbeat)
+      actor(:heartbeat)
+    end
+
+    def add_webhook(actor_name, url)
+      actor(actor_name.to_sym).repository.add(url)
+    end
+
+    def remove_webhook(actor_name, url)
+      actor(actor_name.to_sym).repository.remove(url)
+    end
+
+    def webhooks(actor_name)
+      actor = observer.find(actor_name.to_sym)
+      actor.hooks
     end
 
     def initialize
