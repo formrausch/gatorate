07036140d3255ea8e96465d1811c9122255da742
Thomas Winkler <tom@formrausch.com>
2014-01-13 17:03:08 +0100

    move observer content into gatorate.

--- lib/gatorate.rb
+++ lib/gatorate.rb
@@ -8,7 +8,20 @@ require_relative 'gatorate/pin_actor'
 
 require_relative 'gatorate/door'
 require_relative 'gatorate/heartbeat'
-require_relative 'gatorate/observer'
 
 require_relative 'gatorate/console'
 require_relative 'gatorate/web'
+
+
+# TODO:
+# Trap INT/SIGINT an exit gracefully
+
+module Gatorate
+  class Observer < Celluloid::SupervisionGroup
+    supervise Webhook::Channel, as: :channel_actor
+
+    supervise Gatorate::Door, as: :door, args: [{pin: 0, frequency: 0.1}]
+    supervise Gatorate::Heartbeat, as: :heartbeat, args: [{pin: 17, frequency: 10}]
+  end
+end
+
